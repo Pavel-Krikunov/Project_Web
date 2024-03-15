61ea53420d3290fa1a91a16e533913bb753f7bfd
pvlkr <pvlkrikunov@gmail.com>
2024-03-15 19:23:42 +0300

    redirect complete

--- login.py
+++ login.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('fkasldjfljasd')
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=8, message="Not less than 8 chars")])
+    submit = SubmitField('Correct')
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
-
+import sqlalchemy
 from login import LoginForm
 
 app = Flask(__name__)
@@ -8,10 +8,12 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
-    if form.submit():
-        print('sdljfkjsds')
-    print(form.username.data)
-    print(form.password.data)
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+        if form.username.data == '11111111' and form.password.data == '11111111':
+            return redirect("/about")
+        return 'not now, not yet'
     return render_template('login.html', title='Авторизация', form=form)
 
 
